d2d78fb36791e7d7b5508a68c9fe762910f68380
KirillovE <evgeniy.kirillov@live.ru>
2018-09-30 21:31:22 +0300

    Создан класс для хранения информации о произведении искусства

---
+++ Public Art/Artwork.h
@@ -0,0 +1,26 @@
+//
+//  Artwork.h
+//  Public Art
+//
+//  Created by Евгений Кириллов on 30/09/2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Artwork : NSObject
+
+@property (strong, nonatomic) NSString *title;
+@property (strong, nonatomic) NSString *discipline;
+@property (strong, nonatomic) NSString *creator;
+@property (strong, nonatomic) NSNumber *date;
+@property (strong, nonatomic) NSString *location;
+@property (strong, nonatomic) NSString *artDescription;
+@property (strong, nonatomic) NSNumber *latitude;
+@property (strong, nonatomic) NSNumber *longitude;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Public Art/Artwork.m
@@ -0,0 +1,29 @@
+//
+//  Artwork.m
+//  Public Art
+//
+//  Created by Евгений Кириллов on 30/09/2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+#import "Artwork.h"
+
+@implementation Artwork
+
+- (instancetype)initWithDictionary:(NSDictionary *)dict
+{
+    self = [super init];
+    if (self) {
+        self.title = [dict valueForKey:@"title"];
+        self.discipline = [dict valueForKey:@"discipline"];
+        self.creator = [dict valueForKey:@"creator"];
+        self.date = [dict valueForKey:@"date"];
+        self.location = [dict valueForKey:@"location"];
+        self.artDescription = [dict valueForKey:@"description"];
+        self.latitude = [dict valueForKey:@"latitude"];
+        self.longitude = [dict valueForKey:@"longitude"];
+    }
+    return self;
+}
+
+@end
