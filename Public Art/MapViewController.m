9bd6dff92a38caed35a8ce26779242028f352d5b
KirillovE <evgeniy.kirillov@live.ru>
2018-09-30 21:30:47 +0300

    На первом экране расположилась карта

--- Public Art/MapViewController.m
+++ Public Art/MapViewController.m
@@ -16,7 +16,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    CGRect mapFrame = CGRectMake(0,
+                                 0,
+                                 UIScreen.mainScreen.bounds.size.width,
+                                 UIScreen.mainScreen.bounds.size.height);
+    MKMapView *mapView = [[MKMapView alloc] initWithFrame:mapFrame];
+    [self.view addSubview:mapView];
 }
 
 
