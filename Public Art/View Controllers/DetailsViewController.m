9f9950b20da8cd2cf1e73e6f5841423882a13d92
KirillovE <evgeniy.kirillov@live.ru>
2018-10-01 16:19:27 +0300

    Рефакторинг, документирование, незначительные изменения параметров

--- Public Art/View Controllers/DetailsViewController.m
+++ Public Art/View Controllers/DetailsViewController.m
@@ -14,6 +14,7 @@
 @property (strong, nonatomic) UILabel *artTitle;
 @property (strong, nonatomic) UILabel *artCredits;
 @property (strong, nonatomic) UITextView *artDescription;
+
 @property (strong, nonatomic) NSString *discipline;
 @property (strong, nonatomic) NSString *creator;
 @property (strong, nonatomic) NSString *date;
@@ -26,6 +27,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = UIColor.whiteColor;
+    
     [self addDismissButton];
     [self addArtTitleLabel];
     [self addArtCreditsLabel];
@@ -46,18 +48,20 @@
     [self.dismissButton addTarget:self
                            action:@selector(dismissVC)
                  forControlEvents:UIControlEventTouchUpInside];
+    
     [self.view addSubview:self.dismissButton];
 }
 
 - (void)addArtTitleLabel {
-    if ([self.artwork.title isEqual:[NSNull null]]) {
-        return;
-    }
+    if (!self.artwork.title) { return; }
     
-    self.artTitle = [[UILabel alloc] initWithFrame:CGRectZero];
+    self.artTitle = [UILabel new];
     self.artTitle.text = self.artwork.title;
     self.artTitle.numberOfLines = 0;
+    
     self.artTitle.font = [UIFont systemFontOfSize:20 weight:UIFontWeightSemibold];
+    self.artTitle.textColor = UIColor.blackColor;
+    
     CGSize labelSize = [self getLabelSizeForText:self.artTitle.text
                                         withFont:self.artTitle.font];
     CGRect labelFrame = CGRectMake(18,
@@ -65,7 +69,6 @@
                                    labelSize.width,
                                    labelSize.height);
     self.artTitle.frame = labelFrame;
-    self.artTitle.textColor = UIColor.blackColor;
     
     [self.view addSubview:self.artTitle];
 }
@@ -76,15 +79,17 @@
     [self setDate];
     [self setLocation];
     
-    self.artCredits = [[UILabel alloc] initWithFrame:CGRectZero];
+    self.artCredits = [UILabel new];
     self.artCredits.text = [NSString stringWithFormat:@"%@ by %@, %@ \nat %@",
                             self.discipline,
                             self.creator,
                             self.date,
                             self.location];
-    
     self.artCredits.numberOfLines = 0;
+    
     self.artCredits.font = [UIFont systemFontOfSize:17 weight:UIFontWeightRegular];
+    self.artCredits.textColor = UIColor.darkGrayColor;
+    
     CGSize labelSize = [self getLabelSizeForText:self.self.artCredits.text
                                         withFont:self.self.artCredits.font];
     CGRect labelFrame = CGRectMake(18,
@@ -92,49 +97,65 @@
                                    labelSize.width,
                                    labelSize.height);
     self.artCredits.frame = labelFrame;
-    self.artCredits.textColor = UIColor.blackColor;
     
     [self.view addSubview:self.artCredits];
 }
 
 - (void)addArtDescriptionView {
-    if (!self.artwork.description) {
-        return;
-    }
+    if (!self.artwork.description) { return; }
     
     self.artDescription = [UITextView new];
     self.artDescription.text = self.artwork.artDescription;
+    
+    self.artDescription.font = [UIFont systemFontOfSize:17 weight:UIFontWeightThin];
+    self.artDescription.textColor = UIColor.darkGrayColor;
+    
     CGFloat textViewHeight = self.dismissButton.frame.origin.y - CGRectGetMaxY(self.artCredits.frame) - 20;
     CGRect textViewRect = CGRectMake(18,
                                      CGRectGetMaxY(self.artCredits.frame) + 10,
                                      self.view.bounds.size.width - 36,
                                      textViewHeight);
     self.artDescription.frame = textViewRect;
-    self.artDescription.font = [UIFont systemFontOfSize:17 weight:UIFontWeightThin];
     
     [self.view addSubview:self.artDescription];
 }
 
 #pragma mark - вспомогательное
 
+/**
+ Закрывает текущий View Controller
+ */
 - (void)dismissVC {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+/**
+ Рассчитывает размеры Label
+
+ @param text Текст для отображения на Label
+ @param font Шрифт текста
+ @return Размер Label
+ */
 - (CGSize)getLabelSizeForText:(NSString *)text
                      withFont:(UIFont *)font {
+    
     CGFloat maxWidth = self.view.bounds.size.width - 36;
     CGSize textBlock = CGSizeMake(maxWidth, CGFLOAT_MAX);
+    
     NSDictionary *attributesDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
                                           font, NSFontAttributeName,
                                           nil];
+    
     CGRect rect = [text boundingRectWithSize:textBlock
                                      options:NSStringDrawingUsesLineFragmentOrigin
                                   attributes:attributesDictionary
                                      context:nil];
     
     return CGSizeMake(rect.size.width, rect.size.height);
+    
 }
+
+#pragma mark - проверка значений полей на nil и установка
 
 - (void)setDiscipline {
     if (!self.artwork.discipline) {
@@ -154,7 +175,7 @@
 
 - (void)setDate {
     if ([self.artwork.date isEqual:[NSNull null]]) {
-        self.date = @"";
+        self.date = @"Unknown year";
     } else {
         self.date = self.artwork.date.description;
     }
