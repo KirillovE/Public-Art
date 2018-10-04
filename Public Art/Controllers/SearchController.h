a302676826813813c9353944f1b196c2b60532c3
KirillovE <evgeniy.kirillov@live.ru>
2018-10-04 08:53:43 +0300

    Переименована папка View Controllers, так как в ней теперь содержатся не только View controller'ы. Добавлен контроллер поиска

---
+++ Public Art/Controllers/SearchController.h
@@ -0,0 +1,17 @@
+//
+//  SearchController.h
+//  Public Art
+//
+//  Created by Евгений Кириллов on 04/10/2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SearchController : UISearchController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Public Art/Controllers/SearchController.m
@@ -0,0 +1,21 @@
+//
+//  SearchController.m
+//  Public Art
+//
+//  Created by Евгений Кириллов on 04/10/2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+#import "SearchController.h"
+
+@interface SearchController ()
+
+@end
+
+@implementation SearchController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
