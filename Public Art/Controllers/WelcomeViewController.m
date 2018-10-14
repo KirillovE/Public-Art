77b803b0a4ddfa85317e3c75a8d02ce3ee733d45
KirillovE <evgeniy.kirillov@live.ru>
2018-10-15 01:06:27 +0300

    Добавлена кнопка перехода на главный экран приложения

--- Public Art/Controllers/WelcomeViewController.m
+++ Public Art/Controllers/WelcomeViewController.m
@@ -10,6 +10,8 @@
 
 @interface WelcomeViewController ()
 
+@property (strong, nonatomic) UIButton *nextButton;
+
 @end
 
 @implementation WelcomeViewController
@@ -25,9 +27,16 @@ struct Content {
     
     self.view.backgroundColor = UIColor.cyanColor;
     self.dataSource = self;
-//    self.delegate = self;
+    self.delegate = self;
+    
+    ContentViewController *startViewController = [self viewControllerAtIndex:0];
+    [self setViewControllers:@[startViewController]
+                   direction:UIPageViewControllerNavigationDirectionForward
+                    animated:NO
+                  completion:nil];
     
     [self createContentArray];
+    [self addNextButton];
 }
 
 - (void)createContentArray {
@@ -44,6 +53,41 @@ struct Content {
         content[i].imageName = [NSString stringWithFormat:@"welcome %d", i + 1];
     }
 }
+
+- (void)addNextButton {
+    self.nextButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.nextButton.frame = CGRectMake(self.view.bounds.size.width - 100,
+                                       self.view.bounds.size.height - 50,
+                                       100,
+                                       50);
+    [self.nextButton addTarget:self
+                        action:@selector(nextButtonDidTap:)
+              forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.nextButton setTitle:@"Next" forState:UIControlStateNormal];
+    [self.nextButton setTintColor:[UIColor blackColor]];
+    self.nextButton.hidden = YES;
+    
+    [self updateButtonWithIndex:0];
+    [self.view addSubview:self.nextButton];
+}
+
+- (void)nextButtonDidTap:(UIButton *)sender {
+    NSInteger index = ((ContentViewController *)[self.viewControllers firstObject]).index;
+    if (sender.tag) {
+        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"firstStart"];
+        [self dismissViewControllerAnimated:YES completion:nil];
+    } else {
+        __weak typeof(self) weakSelf = self;
+        [self setViewControllers:@[[self viewControllerAtIndex:index + 1]]
+                       direction:UIPageViewControllerNavigationDirectionForward
+                        animated:YES
+                      completion:^(BOOL finished) {
+                          [weakSelf updateButtonWithIndex:index + 1];
+                      }
+         ];
+    }
+}
     
 - (ContentViewController *)viewControllerAtIndex:(NSInteger)index {
     if (index < 0 || index >= 3) {
@@ -57,6 +101,16 @@ struct Content {
     contentViewController.index = index;
     
     return contentViewController;
+}
+
+- (void)updateButtonWithIndex:(NSInteger)index {
+    if (index < 2) {
+        self.nextButton.hidden = YES;
+        self.nextButton.tag = 0;
+    } else {
+        self.nextButton.hidden = NO;
+        self.nextButton.tag = 1;
+    }
 }
 
 #pragma mark - Page view controller data source
@@ -75,6 +129,20 @@ struct Content {
     NSInteger index = ((ContentViewController *)viewController).index;
     index += 1;
     return [self viewControllerAtIndex:index];
+}
+
+#pragma mark - Page view controller delegate
+
+- (void)pageViewController:(UIPageViewController *)pageViewController
+        didFinishAnimating:(BOOL)finished
+   previousViewControllers:(NSArray<UIViewController *> *)previousViewControllers
+       transitionCompleted:(BOOL)completed {
+    
+    if (completed) {
+        NSInteger index = ((ContentViewController *)
+                           [pageViewController.viewControllers firstObject]).index;
+        [self updateButtonWithIndex:index];
+    }
 }
 
 
