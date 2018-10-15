f10dc08c4290c9ed7a34d462943f73db484e6885
KirillovE <evgeniy.kirillov@live.ru>
2018-10-15 13:35:40 +0300

    Добавлен контроллер для отображения избранных артефактов

---
+++ Public Art/Controllers/FavoritesCollectionViewController.h
@@ -0,0 +1,18 @@
+//
+//  FavoritesCollectionViewController.h
+//  Public Art
+//
+//  Created by Евгений Кириллов on 15/10/2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ArtefactsCollectionViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FavoritesCollectionViewController : ArtefactsCollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Public Art/Controllers/FavoritesCollectionViewController.m
@@ -0,0 +1,17 @@
+//
+//  FavoritesCollectionViewController.m
+//  Public Art
+//
+//  Created by Евгений Кириллов on 15/10/2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+#import "FavoritesCollectionViewController.h"
+
+@interface FavoritesCollectionViewController ()
+
+@end
+
+@implementation FavoritesCollectionViewController
+
+@end
